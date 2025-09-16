ec7c45e4bcea2270f8f2161216cb0ff0f70682ed
petraalpigiano <petraalpigiano@gmail.com>
2025-09-16 11:50:24 +0200

    add api call for show2

--- src/pages/albums/AlbumDetailsPage.jsx
+++ src/pages/albums/AlbumDetailsPage.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function AlbumDetailsPage() {
+  const { id } = useParams();
+  const [APIAlbum, setAPIAlbum] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/albums/" + id).then((response) => {
+      const specificAlbum = response.data.data;
+      setAPIAlbum(specificAlbum);
+    });
+  }, []);
+  console.log(APIAlbum);
+
   return (
     <>
-      <h1>Sono la show</h1>
+      <div className="container text-center my-4">
+        {APIAlbum.image != null && (
+          <img
+            src={`/${APIAlbum.image}`}
+            className="card-img-top img-custom2 mb-2"
+            alt={APIAlbum.name}
+          />
+        )}
+        <h3>{APIAlbum.name}</h3>
+        <p>{APIAlbum.published_year}</p>
+        <p>Songs : {APIAlbum.n_songs}</p>
+        {APIAlbum.genre != null && <p>Genere: {APIAlbum.genre.name}</p>}
+      </div>
     </>
   );
 }
--- src/pages/albums/AlbumsListPage.jsx
+++ src/pages/albums/AlbumsListPage.jsx
@@ -3,32 +3,35 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function AlbumListPage() {
-  const [APIData, setAPIData] = useState([]);
+  const [APIAlbums, setAPIAlbums] = useState([]);
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/albums").then((response) => {
       const albums = response.data.data;
       //   console.log(albums);
-      setAPIData(albums);
+      setAPIAlbums(albums);
     });
   }, []);
-  console.log(APIData);
+  //   console.log(APIAlbums);
 
   return (
     <>
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4 my-1">
-          {APIData.map(function (currentAlbum) {
+          {APIAlbums.map(function (currentAlbum) {
             return (
               <div className="col" key={currentAlbum.id}>
                 <Link
                   to={`/albums/${currentAlbum.id}`}
                   className="card h-100 link"
                 >
-                  <img
-                    src={`src/assets/${currentAlbum.image}`}
-                    className="card-img-top"
-                    alt={currentAlbum.name}
-                  />
+                  {currentAlbum.image != null && (
+                    <img
+                      src={`/${currentAlbum.image}`}
+                      className="card-img-top"
+                      alt={currentAlbum.name}
+                    />
+                  )}
+
                   <div className="card-body text-center ">
                     <h5 className="card-title ">{currentAlbum.name}</h5>
                     <p className="card-text">{currentAlbum.published_year}</p>
